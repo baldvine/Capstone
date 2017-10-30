4912295961f43d6088aeff15e63bd774bf2af027
Baldvin Einarsson <beinarsson@air-worldwide.com>
2017-10-30 10:36:26 -0400

    fixed path issue in getData.R

--- getData.R
+++ getData.R
@@ -1,6 +1,6 @@
 
 fileURL <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/"
-fileZip <- "Coursera-SwiftKey.zip"
+fileZip <- "data/Coursera-SwiftKey.zip"
 
 if (!file.exists(fileZip)){
     download.file(url=fileURL, destfile = fileZip, method='curl')
